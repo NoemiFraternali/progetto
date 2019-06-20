cd2a93d47d15c27fd924ebf31ed4c246386b027f
NoemiFraternali <48881208+NoemiFraternali@users.noreply.github.com>
2019-06-20 18:00:44 +0200

    Update HomeController.java

--- progetto-siw/src/main/java/it/uniroma3/spring/progettosiw/controller/HomeController.java
+++ progetto-siw/src/main/java/it/uniroma3/spring/progettosiw/controller/HomeController.java
@@ -1,29 +1,18 @@
 package it.uniroma3.spring.progettosiw.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.Date;
 
 @Controller
 public class HomeController {
     
-	private String appMode;
 
-    @Autowired
-    public HomeController(Environment environment){
-        appMode = environment.getProperty("app-mode");
-    }
+
 
     @RequestMapping("/")
     public String index(Model model){
-        model.addAttribute("username", "@SilphSPA");
-        model.addAttribute("projectname", "Home");
 
-        model.addAttribute("mode", appMode);
 
         return "index";
     }
