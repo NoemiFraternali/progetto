cef8006923c349cb888f4014cc1918768e35b22d
EricaGarg <51815762+EricaGarg@users.noreply.github.com>
2019-06-20 17:48:01 +0200

    Update Amministratore.java

--- progetto-siw/src/main/java/it/uniroma3/spring/progettosiw/model/Amministratore.java
+++ progetto-siw/src/main/java/it/uniroma3/spring/progettosiw/model/Amministratore.java
@@ -1,7 +1,5 @@
 package it.uniroma3.spring.progettosiw.model;
 
-
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,69 +7,57 @@ import javax.persistence.Id;
 
 @Entity
 public class Amministratore {
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
-	private String nome;
-	private String cognome;
-	private String email;
-	private String username;
-	private String password;
-
 	
-	public Amministratore(Long id, String nome, String cognome, String email, String username, String password) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;	
+	private String username;	
+	private String password;	
+	private String email;
+	
+	public Amministratore(Long id, String username, String password, String email) {
 		super();
 		this.id = id;
-		this.nome = nome;
-		this.cognome = cognome;
-		this.email = email;
 		this.username = username;
 		this.password = password;
-	}
-
-
+		this.email = email;
+	}	
+	
 	public enum Role {
 		ROLE_ADMIN
 	}
-	
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNome() {
-		return nome;
+
+	public String getUsername() {
+		return username;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public String getCognome() {
-		return cognome;
+	public String getEmail() {
+		return email;
 	}
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+	
 	
 }
